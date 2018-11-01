c8fb198b7edf38fbdef10894dca3611dd79554c1
vartanbeno <vartanbeno@gmail.com>
2018-11-01 13:40:34 -0400

    add User class

---
+++ src/dom/model/user/User.java
@@ -0,0 +1,45 @@
+package dom.model.user;
+
+public class User {
+	
+	private long id;
+	private int version;
+	private String username;
+	private String password;
+	
+	public User(long id, int version, String username, String password) {
+		this.id = id;
+		this.version = version;
+		this.username = username;
+		this.password = password;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public int getVersion() {
+		return version;
+	}
+	
+	public void setVersion(int version) {
+		this.version = version;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
