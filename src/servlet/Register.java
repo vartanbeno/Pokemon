9762e6d4947b038b5e270a6ea4235c03167d5846
vartanbeno <vartanbeno@gmail.com>
2018-11-02 00:21:23 -0400

    saving users to db

--- src/servlet/Register.java
+++ src/servlet/Register.java
@@ -10,15 +10,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.dsrg.soenea.service.MySQLConnectionFactory;
+import org.dsrg.soenea.service.threadLocal.DbRegistry;
+
+import dom.model.user.rdg.UserRDG;
+
 @WebServlet("/Register")
 public class Register extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private static final String SUCCESS_JSP = "WEB-INF/jsp/success.jsp";
-	private static final String FAILURE_JSP = "WEB-INF/jsp/fail.jsp";
+	private static final String SUCCESS_JSP = "/WEB-INF/jsp/success.jsp";
+	private static final String FAILURE_JSP = "/WEB-INF/jsp/fail.jsp";
 	
 	public static Map<String, String>registeredMap = new HashMap<String, String>(); 
+	
+	@Override
+	public void init(javax.servlet.ServletConfig config) throws ServletException {
+		try {
+			String key = "";
+			MySQLConnectionFactory connectionFactory = new MySQLConnectionFactory(null, null, null, null);
+			connectionFactory.defaultInitialization();
+			DbRegistry.setConFactory(key, connectionFactory);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
     public Register() {
         super();
@@ -26,21 +43,30 @@ public class Register extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String username = request.getParameter("user");
-		String password = request.getParameter("pass");
-		
-		if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
-			request.setAttribute("message", "Please enter both a username and a password.");
-			request.getRequestDispatcher(FAILURE_JSP).forward(request, response);
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			
+			String username = request.getParameter("user");
+			String password = request.getParameter("pass");
+			
+			if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
+				request.setAttribute("message", "Please enter both a username and a password.");
+				request.getRequestDispatcher(FAILURE_JSP).forward(request, response);
+			}
+			else if (UserRDG.findByUsername(username) != null) {
+				request.setAttribute("message", "This username is taken.");
+				request.getRequestDispatcher(FAILURE_JSP).forward(request, response);
+			}
+			else {
+				new UserRDG(UserRDG.getMaxId(), 1, username, password).insert();
+				request.setAttribute("message", "Successfully registered.");
+				request.getRequestDispatcher(SUCCESS_JSP).forward(request, response);
+			}
+			
 		}
-		else if (registeredMap.containsKey(username)) {
-			request.setAttribute("message", "This username is taken.");
-			request.getRequestDispatcher(FAILURE_JSP).forward(request, response);
-		}
-		else {
-			registeredMap.put(username, password);
-			request.setAttribute("message", "Successfully registered.");
-			request.getRequestDispatcher(SUCCESS_JSP).forward(request, response);
+		catch (Exception e) {
+			e.printStackTrace();
 		}
 		
 	}
