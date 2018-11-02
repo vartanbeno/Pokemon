3a82ac7057f250dd46b44e815e2a11f67b6eb6a4
vartanbeno <vartanbeno@gmail.com>
2018-11-02 14:16:57 -0400

    Add Logout servlet
    Signed-off-by: vartanbeno <vartanbeno@gmail.com>

---
+++ src/servlet/Logout.java
@@ -0,0 +1,47 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dom.model.user.rdg.UserRDG;
+
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private static final String SUCCESS_JSP = "/WEB-INF/jsp/success.jsp";
+
+    public Logout() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			
+			long id = (long) request.getSession(true).getAttribute("userid");
+			UserRDG userRDG = UserRDG.findById(id);
+			
+			request.getSession(true).invalidate();
+			request.setAttribute("message", String.format("User %s has successfully logged out.", userRDG.getUsername()));
+			request.getRequestDispatcher(SUCCESS_JSP).forward(request, response);
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
