3e84d1fa51d8651e0be2c0c35955eba9b07af43c
vartanbeno <vartanbeno@gmail.com>
2018-11-04 17:21:16 -0500

    Better error handling for Register/Login
    Signed-off-by: vartanbeno <vartanbeno@gmail.com>

--- src/servlet/Login.java
+++ src/servlet/Login.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dom.model.user.UserHelper;
 import dom.model.user.rdg.UserRDG;
 
 @WebServlet("/Login")
@@ -19,27 +20,65 @@ public class Login extends PageController {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher(Global.LOGIN_FORM).forward(request, response);
+		
+		Long userId = null;
+		
+		try {
+			
+			userId = (long) request.getSession(true).getAttribute("userid");
+			
+			UserRDG userRDG = null;
+
+			try {
+				userRDG = UserRDG.findById(userId);
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			UserHelper user = new UserHelper(userRDG.getId(), userRDG.getVersion(), userRDG.getUsername(), "");
+			failure(request, response, String.format("You are already logged in as %s.", user.getUsername()));
+		
+		}
+		catch (NullPointerException e) {
+			request.getRequestDispatcher(Global.LOGIN_FORM).forward(request, response);
+		}
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		try {
-						
-			String username = request.getParameter("user");
-			String password = request.getParameter("pass");
 			
+			Long userId = null;
 			UserRDG userRDG = null;
 			
-			if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
-				failure(request, response, "Please enter both a username and a password.");
+			try {
+				
+				userId = (long) request.getSession(true).getAttribute("userid");
+				
+				userRDG = UserRDG.findById(userId);
+				UserHelper user = new UserHelper(userRDG.getId(), userRDG.getVersion(), userRDG.getUsername(), "");
+				
+				failure(request, response, String.format("You are already logged in as %s.", user.getUsername()));
+				
 			}
-			else if ((userRDG = UserRDG.findByUsernameAndPassword(username, password)) != null) {
-				request.getSession(true).setAttribute("userid", userRDG.getId());
-				success(request, response, "Successfully logged in.");
-			}
-			else {
-				failure(request, response, "Incorrect username and/or password.");
+			catch (NullPointerException e) {
+				
+				String username = request.getParameter("user");
+				String password = request.getParameter("pass");
+				
+				if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
+					failure(request, response, "Please enter both a username and a password.");
+				}
+				else if ((userRDG = UserRDG.findByUsernameAndPassword(username, password)) != null) {
+					request.getSession(true).setAttribute("userid", userRDG.getId());
+					success(request, response, "Successfully logged in.");
+				}
+				else {
+					failure(request, response, "Incorrect username and/or password.");
+				}
+				
 			}
 			
 		}
--- src/servlet/Register.java
+++ src/servlet/Register.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dom.model.user.UserHelper;
 import dom.model.user.rdg.UserRDG;
 
 @WebServlet("/Register")
@@ -19,29 +20,67 @@ public class Register extends PageController {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher(Global.REGISTER_FORM).forward(request, response);
+		
+		Long userId = null;
+		
+		try {
+			
+			userId = (long) request.getSession(true).getAttribute("userid");
+			
+			UserRDG userRDG = null;
+
+			try {
+				userRDG = UserRDG.findById(userId);
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			UserHelper user = new UserHelper(userRDG.getId(), userRDG.getVersion(), userRDG.getUsername(), "");
+			failure(request, response, String.format("You are already logged in as %s.", user.getUsername()));
+			
+		}
+		catch (NullPointerException e) {
+			request.getRequestDispatcher(Global.REGISTER_FORM).forward(request, response);
+		}
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		try {
-						
-			String username = request.getParameter("user");
-			String password = request.getParameter("pass");
 			
+			Long userId = null;
 			UserRDG userRDG = null;
 			
-			if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
-				failure(request, response, "Please enter both a username and a password.");
+			try {
+				
+				userId = (long) request.getSession(true).getAttribute("userid");
+				
+				userRDG = UserRDG.findById(userId);
+				UserHelper user = new UserHelper(userRDG.getId(), userRDG.getVersion(), userRDG.getUsername(), "");
+				
+				failure(request, response, String.format("You are already logged in as %s.", user.getUsername()));
+				
 			}
-			else if ((userRDG = UserRDG.findByUsername(username)) != null) {
-				failure(request, response, "This username is taken.");
-			}
-			else {
-				userRDG = new UserRDG(UserRDG.getMaxId(), 1, username, password);
-				userRDG.insert();
-				request.getSession(true).setAttribute("userid", userRDG.getId());
-				success(request, response, "Successfully registered.");
+			catch (NullPointerException e) {
+				
+				String username = request.getParameter("user");
+				String password = request.getParameter("pass");
+				
+				if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
+					failure(request, response, "Please enter both a username and a password.");
+				}
+				else if ((userRDG = UserRDG.findByUsername(username)) != null) {
+					failure(request, response, "This username is taken.");
+				}
+				else {
+					userRDG = new UserRDG(UserRDG.getMaxId(), 1, username, password);
+					userRDG.insert();
+					request.getSession(true).setAttribute("userid", userRDG.getId());
+					success(request, response, "Successfully registered.");
+				}
+				
 			}
 			
 		}
