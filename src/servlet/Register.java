468899d7b21fb08ae6e4e70381870cbe2df31440
vartanbeno <vartanbeno@gmail.com>
2018-11-06 11:50:32 -0500

    Some tweaking to Register servlet
    Declaring variables to contain the success/fail messages.
    If a user tries to register with an existing username, show the username in the fail message.
    
    Signed-off-by: vartanbeno <vartanbeno@gmail.com>

--- src/servlet/Register.java
+++ src/servlet/Register.java
@@ -16,6 +16,10 @@ public class Register extends PageController {
 	private static final long serialVersionUID = 1L;
 	
 	private static final String ALREADY_LOGGED_IN = "You are already logged in as %s.";
+	private static final String ENTER_USER_AND_PASS = "Please enter both a username and a password.";
+	private static final String USERNAME_TAKEN = "The username %s is taken.";
+	
+	private static final String REGISTRATION_SUCCESS = "Successfully registered.";
 
     public Register() {
         super();
@@ -61,16 +65,16 @@ public class Register extends PageController {
 				String password = request.getParameter("pass");
 				
 				if (username == null || password == null || username.trim().isEmpty() || password.trim().isEmpty()) {
-					failure(request, response, "Please enter both a username and a password.");
+					failure(request, response, ENTER_USER_AND_PASS);
 				}
 				else if ((userRDG = UserRDG.findByUsername(username)) != null) {
-					failure(request, response, "This username is taken.");
+					failure(request, response, String.format(USERNAME_TAKEN, username));
 				}
 				else {
 					userRDG = new UserRDG(UserRDG.getMaxId(), 1, username, password);
 					userRDG.insert();
 					request.getSession(true).setAttribute("userid", userRDG.getId());
-					success(request, response, "Successfully registered.");
+					success(request, response, REGISTRATION_SUCCESS);
 				}
 				
 			}
